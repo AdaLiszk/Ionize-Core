a85154d8e0555a0ca8017760328fed1585869891
Ádám Liszkai <adaliszk@gmail.com>
2017-05-11 00:19:58 +0200

    add:Sample Template

---
+++ public/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title> {{ $content->title }} </title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,500" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,300i,400,600,600i" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/template.css" />
+</head>
+<body>
+
+    <h1>Hello Ionize !</h1>
+    <pre>@php print_r($content) @endphp</pre>
+
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once(__DIR__.'/../vendor/autoload.php');
 
+ini_set('display_errors','On');
+error_reporting(-1);
+
 $app = new Ionize\Application();
 
 /*
