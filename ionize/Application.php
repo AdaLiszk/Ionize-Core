824ee4c8f8f71c009c479ff41face5cedc7691a4
Ádám Liszkai <adaliszk@gmail.com>
2017-05-11 00:19:39 +0200

    add:Singletons & Logger

--- ionize/Application.php
+++ ionize/Application.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Debug\ExceptionHandler;
 use Laravel\Lumen\Application as LumenApplication;
 use Dotenv\Exception\InvalidPathException;
 use Dotenv\Dotenv;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Logger;
 
 /**
  * Ionize Application Class
@@ -65,9 +68,53 @@ class Application extends LumenApplication
             $this->singleton(Kernel::class, Console\Kernel::class);
         }
 
+        // Registerion Ionize Singletons
+        $this->singleton(Illuminate\Urls\Urls::class, Database\Urls\UrlRepository::class);
+        $this->singleton(Illuminate\Contents\Contents::class, Database\Contents\ContentRepository::class);
+
         //$this->register(Providers\ApplicationService::class);
         //$this->register(Providers\EventService::class);
         //$this->register(Providers\AuthService::class);
+    }
+
+    /**
+     * Register container bindings for the application.
+     *
+     * @return void
+     */
+    protected function registerLogBindings()
+    {
+        $this->singleton('Psr\Log\LoggerInterface', function () {
+            return new Logger('Ionize', $this->getMonologHandler());
+        });
+    }
+
+    /**
+     * Extends the default logging implementation with additional handlers if configured in .env
+     *
+     * @return array of type \Monolog\Handler\AbstractHandler
+     */
+    protected function getMonologHandler()
+    {
+        $handlers = [];
+
+        $fileName = date('Y-m-d');
+
+        $streamHandler = new StreamHandler(
+            storage_path('logs/'.$fileName.'.log'),
+            Logger::DEBUG
+        );
+
+        $handlers[] = $streamHandler->setFormatter(
+            new LineFormatter(
+                null,
+                'H:i:s',
+                true,
+                true
+            )
+        );
+
+        return $handlers;
     }
 
     /**
